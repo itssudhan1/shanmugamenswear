82de77a099d8d29a97830fc09eccac2082f5405a
itssudhan1 <itssudhan1@gmail.com>
2026-05-24 11:49:25 +0530

    🛍️ Update products (2 items) — 24/5/2026

--- smw-products.js
+++ smw-products.js
@@ -1,22 +1,23 @@
-/* Shanmuga Mens Wear Products — Published 23/5/2026, 7:08:11 pm */
+/* Shanmuga Mens Wear Products — 2 items — Updated 24/5/2026, 11:49:22 am */
+/* © 2026 Shanmuga Mens Wear. All rights reserved. */
 
 var SMW_DEFAULT_PRODUCTS = [
   {
-    "id": 1774705812284,
-    "name": "blue oxford shirt",
+    "id": 100,
+    "name": "Classic Blue oxford shirt",
     "cat": "shirts",
     "tagline": "premium cotton",
     "description": "",
     "fabric": "100% cotton",
-    "fit": "Regular fit",
+    "fit": "regular fit",
     "orig": 1199,
     "sale": 899,
-    "stock": 6,
+    "stock": 5,
     "threshold": 5,
     "status": "instock",
     "visible": true,
     "lowStock": false,
-    "badge": "new",
+    "badge": "",
     "sizes": [
       "S",
       "M",
@@ -24,10 +25,40 @@ var SMW_DEFAULT_PRODUCTS = [
       "XL"
     ],
     "photos": [
-      "https://res.cloudinary.com/do2topls5/image/upload/v1775796181/s6xd0kaakrmxlndfj4to.webp",
-      "https://res.cloudinary.com/do2topls5/image/upload/v1775796220/yt1qu29eyv4qbrfvh9iv.webp",
-      "https://res.cloudinary.com/do2topls5/image/upload/v1775796224/hwedkremsbe6jkmv4r41.webp",
-      "https://res.cloudinary.com/do2topls5/image/upload/v1775796229/vg6ovnb3if2wif5dlmfo.webp"
+      "https://res.cloudinary.com/do2topls5/image/upload/v1775839926/dliscmryhwvfye5q2nnv.webp",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1775839929/dgqraelrtkczyacikyrb.webp",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1775839949/yv4vybbbmn5pxstenr5p.webp",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1775839955/avy2rxwl04gg6xvdt4nm.webp"
+    ]
+  },
+  {
+    "id": 101,
+    "name": "Beige linen pant",
+    "cat": "pants",
+    "tagline": "premium cotton",
+    "description": "",
+    "fabric": "100% cotton",
+    "fit": "regular fit",
+    "orig": 1299,
+    "sale": 899,
+    "stock": 0,
+    "threshold": 5,
+    "status": "instock",
+    "visible": true,
+    "lowStock": false,
+    "badge": "",
+    "sizes": [
+      "28",
+      "30",
+      "32",
+      "34",
+      "36"
+    ],
+    "photos": [
+      "https://res.cloudinary.com/do2topls5/image/upload/v1777637789/tuebbxwdcj8uxovgnvfg.webp",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1777637816/v0x46fm2lk56klrq8gq6.jpg",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1777637827/hquiozogvibaoh2z98yz.jpg",
+      "https://res.cloudinary.com/do2topls5/image/upload/v1777637861/hsfsbwbemzfaa76o0l98.webp"
     ]
   }
 ];
