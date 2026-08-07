069ef4827e3fca83c127afc0ad70361de6273ba9
itssudhan1 <itssudhan1@gmail.com>
2026-08-07 18:44:22 +0530

    Update smw-banners.js — 7/8/2026

--- smw-banners.js
+++ smw-banners.js
@@ -1,4 +1,4 @@
-/* Shanmuga Mens Wear Banners — 1 items — Updated 6/8/2026, 10:16:17 pm */
+/* Shanmuga Mens Wear Banners — 1 items — Updated 7/8/2026, 6:44:17 pm */
 
 var SMW_DEFAULT_BANNERS = [
   {
@@ -8,7 +8,7 @@ var SMW_DEFAULT_BANNERS = [
     "subtext": "shop our sale collection before stock run out",
     "btnText": "Shop Sale",
     "btnLink": "#shop=tshirts",
-    "bg": "https://t3.ftcdn.net/jpg/04/67/96/14/360_F_467961418_UnS1ZAwAqbvVVMKExxqUNi0MUFTEJI83.jpg",
+    "bg": "",
     "active": true,
     "order": 0
   }
